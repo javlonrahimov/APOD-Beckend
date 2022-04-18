7a2e99d9ce911684b07bcf3316875e50e22fd4ca
javlon <javlonrahimov1212@gmail.com>
2022-04-18 11:11:25 +0500

    fix

--- internal/mock/apods.go
+++ internal/mock/apods.go
@@ -2,26 +2,19 @@ package mock
 
 import (
 	"javlonrahimov/apod/internal/data"
+	"strings"
 )
 
 type apodModelMock struct{}
+
+var apods = make([]data.Apod, 0)
 
 func NewApodsMock() data.ApodService {
 	return &apodModelMock{}
 }
 
 func (a *apodModelMock) Insert(apod *data.Apod) error {
-	// query := `
-	// INSERT INTO apods (title, explanation, date, media_type, url, hd_url)
-	// VALUES ($1, $2, $3, $4, $5, $6)
-	// RETURNING id, version`
-
-	// args := []interface{}{apod.Title, apod.Explanation, apod.Date, apod.MediaType, apod.Url, apod.HdUrl}
-
-	// ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	// defer cancel()
-
-	// return a.db.QueryRowContext(ctx, query, args...).Scan(&apod.ID, &apod.Version)
+	apods = append(apods, *apod)
 	return nil
 }
 
@@ -31,37 +24,13 @@ func (a *apodModelMock) GetById(id int64) (*data.Apod, error) {
 		return nil, data.ErrRecordNotFound
 	}
 
-	// query := `
-	// SELECT id, title, explanation, date, media_type, url, hd_url, version
-	// FROM apods WHERE id = $1`
+	for i := 0; i < len(apods); i++ {
+		if apods[i].ID == id {
+			return &apods[i], nil
+		}
+	}
 
-	// var apod Apod
-
-	// ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	// defer cancel()
-
-	// err := a.db.QueryRowContext(ctx, query, id).Scan(
-	// 	&apod.ID,
-	// 	&apod.Title,
-	// 	&apod.Explanation,
-	// 	&apod.Date,
-	// 	&apod.MediaType,
-	// 	&apod.Url,
-	// 	&apod.HdUrl,
-	// 	&apod.Version,
-	// )
-
-	// if err != nil {
-	// 	switch {
-	// 	case errors.Is(err, sql.ErrNoRows):
-	// 		return nil, data.ErrRecordNotFound
-	// 	default:
-	// 		return nil, err
-	// 	}
-	// }
-
-	// return &apod, nil
-	return nil, nil
+	return nil, data.ErrRecordNotFound
 }
 
 func (a *apodModelMock) GetByDate(date string) (*data.Apod, error) {
@@ -70,71 +39,24 @@ func (a *apodModelMock) GetByDate(date string) (*data.Apod, error) {
 		return nil, data.ErrRecordNotFound
 	}
 
-	// query := `
-	// SELECT id, title, explanation, date, media_type, url, hd_url, version
-	// FROM apods WHERE id = $1`
+	for i := 0; i < len(apods); i++ {
+		if apods[i].Date == date {
+			return &apods[i], nil
+		}
+	}
 
-	// var apod Apod
-
-	// ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	// defer cancel()
-
-	// err := a.db.QueryRowContext(ctx, query, date).Scan(
-	// 	&apod.ID,
-	// 	&apod.Title,
-	// 	&apod.Explanation,
-	// 	&apod.Date,
-	// 	&apod.MediaType,
-	// 	&apod.Url,
-	// 	&apod.HdUrl,
-	// 	&apod.Version,
-	// )
-
-	// if err != nil {
-	// 	switch {
-	// 	case errors.Is(err, sql.ErrNoRows):
-	// 		return nil, ErrRecordNotFound
-	// 	default:
-	// 		return nil, err
-	// 	}
-	// }
-
-	// return &apod, nil
-
-	return nil, nil
+	return nil, data.ErrRecordNotFound
 }
 
 func (a *apodModelMock) Update(apod *data.Apod) error {
 
-	// query := `
-	// UPDATE apods
-	// SET title = $1, explanation = $2, date = $3, media_type = $4, url = $6, hd_url = $7, version = version + 1
-	// WHERE id = $8 AND version = $9
-	// RETURNING version`
+	for i := 0; i < len(apods); i++ {
+		if apods[i].ID == apod.ID {
+			apods[i] = *apod
+			return nil
+		}
+	}
 
-	// args := []interface{}{
-	// 	apod.Title,
-	// 	apod.Explanation,
-	// 	apod.Date,
-	// 	apod.MediaType,
-	// 	apod.Url,
-	// 	apod.HdUrl,
-	// 	apod.ID,
-	// 	apod.Version,
-	// }
-
-	// ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	// defer cancel()
-
-	// err := a.db.QueryRowContext(ctx, query, args...).Scan(&apod.Version)
-	// if err != nil {
-	// 	switch {
-	// 	case errors.Is(err, sql.ErrNoRows):
-	// 		return ErrEditConflict
-	// 	default:
-	// 		return err
-	// 	}
-	// }
 	return nil
 }
 
@@ -143,80 +65,29 @@ func (m *apodModelMock) Delete(id int64) error {
 		return data.ErrRecordNotFound
 	}
 
-	// query := `
-	// DELETE FROM apods
-	// WHERE id = $1`
+	for i := 0; i < len(apods); i++ {
+		if apods[i].ID == id {
+			apods = removeApod(apods, i)
+			return nil
+		}
+	}
 
-	// ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	// defer cancel()
-
-	// result, err := m.db.ExecContext(ctx, query, id)
-	// if err != nil {
-	// 	return err
-	// }
-
-	// rowsAffected, err := result.RowsAffected()
-	// if err != nil {
-	// 	return err
-	// }
-
-	// if rowsAffected == 0 {
-	// 	return ErrRecordNotFound
-	// }
-
-	return nil
+	return data.ErrRecordNotFound
 }
 
 func (a *apodModelMock) GetAll(title string, filters data.Filters) ([]*data.Apod, data.Metadata, error) {
-	// query := fmt.Sprintf(`
-	// 	SELECT count(*) OVER(), id, created_at, title, year, runtime, genres, version
-	// 	FROM movies
-	// 	WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '')
-	// 	ORDER BY %s %s, id ASC
-	// 	LIMIT $2 OFFSET $3`, filters.SortColumn(), filters.SortDirection())
 
-	// ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	// defer cancel()
+	pagingApods := make([]*data.Apod, 0)
 
-	// args := []interface{}{title, filters.Limit(), filters.Offset()}
+	for i := 0; i < len(apods); i++ {
+		if strings.Contains(apods[i].Title, title) {
+			pagingApods = append(pagingApods, &apods[i])
+		}
+	}
 
-	// rows, err := a.db.QueryContext(ctx, query, args...)
-	// if err != nil {
-	// 	return nil, Metadata{}, err
-	// }
-
-	// defer rows.Close()
-
-	// totalRecords := 0
-	// apods := []*Apod{}
-
-	// for rows.Next() {
-	// 	var apod Apod
-
-	// 	err := rows.Scan(
-	// 		&totalRecords,
-	// 		&apod.ID,
-	// 		&apod.Title,
-	// 		&apod.Explanation,
-	// 		&apod.MediaType,
-	// 		&apod.Date,
-	// 		&apod.Url,
-	// 		&apod.HdUrl,
-	// 		&apod.Version,
-	// 	)
-
-	// 	if err != nil {
-	// 		return nil, Metadata{}, err
-	// 	}
-
-	// 	apods = append(apods, &apod)
-	// }
-
-	// metadata := calculateMetadata(totalRecords, filters.Page, filters.PageSize)
-
-	// return apods, metadata, nil
-
-	return nil, data.Metadata{}, nil
+	return pagingApods, data.Metadata{}, nil
 }
 
-//TODO: write validation
+func removeApod(slice []data.Apod, s int) []data.Apod {
+	return append(slice[:s], slice[s+1:]...)
+}
